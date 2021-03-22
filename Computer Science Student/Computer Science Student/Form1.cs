1b84795569dd55e93c1605e12c5a6e2a9af565b8
karinagarces <77650293+karinagarces@users.noreply.github.com>
2021-03-22 19:15:46 -0400

    Add files via upload

---
+++ Computer Science Student/Computer Science Student/Form1.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace Computer_Science_Student
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void GetHoursButton_Click(object sender, EventArgs e)
+        {
+            string name, id, track;
+
+            name = nameTextBox.Text;
+            id = idTextBox.Text;
+
+            if (infoSystemsRadioButton.Checked)
+            {
+                track = "Information Systems";
+            } else
+            {
+                track = "Software Engineering";
+            }
+
+            CompSciStudent csStudent = new CompSciStudent(name, id, track);
+            requiredHoursLabel.Text = csStudent.RequiredHours.ToString("n1");
+        }
+
+        private void ExitButton_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
